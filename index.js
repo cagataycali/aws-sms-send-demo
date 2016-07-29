9d05312180fd387592453080be41a461bb868ec2
Çağatay ÇALI <me@calicagatay.me>
2016-07-29 14:38:13 -0700

    Init

---
+++ index.js
@@ -0,0 +1,16 @@
+var Sender = require('aws-sms-send');
+
+var config = {
+  AWS: {
+    accessKeyId: 'xxxxxxxxxx',
+    secretAccessKey: 'xxxxxxxxxx',
+    region: 'xxxxxxxxxx',
+  },
+  topicArn: 'xxxxxxxxxx',
+};
+var sender = new Sender(config);
+
+// sender.createSubscribe('+905054146201');
+// sender.sendSms('Sms body topic', 'Topic sms', true);
+// sender.sendSms('Sms body direct', 'Topic sms', false, '+905054146201');
+// TODO : @cagatay, please read doc.
